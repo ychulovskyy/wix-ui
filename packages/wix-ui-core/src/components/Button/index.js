46f6ccfc5b8bff6a32b429a3b2633ce5e8538ba2
Lior Belinsky <liorbe@wix.com>
2017-11-26 09:55:54 +0200

    wix-ui-core: (feat)add Button component (wip)

---
+++ packages/wix-ui-core/src/components/Button/index.js
@@ -0,0 +1,5 @@
+import Button from './Button';
+import {styles} from './styles';
+import {withClasses} from '../../theme';
+
+export default withClasses(Button, styles);
--- packages/wix-ui-core/stories/index.js
+++ packages/wix-ui-core/stories/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import {storiesOf} from '@storybook/react';
 import ToggleSwitch from '../src/components/ToggleSwitch';
+import Button from '../src/components/Button';
 
 storiesOf('Components', module)
+  .add('Button', () => (
+    <Button>Hello</Button>
+  ))
   .add('ToggleSwitch', () => (
     <ToggleSwitch/>
   ));
