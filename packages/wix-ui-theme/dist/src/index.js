7d80ae67d8b76ca9716e93f3352db718d6819364
Lior Belinsky <liorbe@wix.com>
2017-11-29 22:38:53 +0200

    wix-ui-test-utils: migrate to jest

--- packages/wix-ui-theme/dist/src/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var ThemedComponent_1 = require("./ThemedComponent");
-exports.ThemedComponent = ThemedComponent_1.ThemedComponent;
-//# sourceMappingURL=index.js.map
