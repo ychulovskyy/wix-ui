308479377faf21a452e046530f00a809922aec12
Lior Belinsky <liorbe@wix.com>
2017-11-29 11:23:24 +0200

    wix-ui-theme: cleanup

--- packages/wix-ui-theme/package/src/ThemedComponent/ThemedComponent.spec.tsx
+++
@@ -1,8 +0,0 @@
-import 'mocha';
-import {expect} from 'chai';
-
-describe('', () => {
-  it('', () => {
-    expect(true).to.equal(true);
-  });
-});
--- packages/wix-ui-theme/package/src/index.ts
+++
@@ -1,1 +0,0 @@
-export {ThemedComponent} from './ThemedComponent';
