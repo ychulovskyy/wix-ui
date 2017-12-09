428eb3fb80e846590f21ad04fc738264cbb7fcfe
Shlomi Toussia-Cohen <shlomitc@gmail.com>
2017-12-10 00:50:28 +0200

    Remove redundant packages (#37)
    * remove haste-cli
    
    * remove react-test-utils
    
    * remove jsdom (comes with jest)
    
    * remove chai

--- packages/wix-storybook-utils/src/FormComponents/index.spec.js
+++ packages/wix-storybook-utils/src/FormComponents/index.spec.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import {shallow} from 'enzyme';
-import {expect} from 'chai';
 
 import {Wrapper} from './';
 
 describe('Wrapper component', () => {
   it('should render children', () => {
     const wrapper = shallow(<Wrapper>hello world</Wrapper>);
-    expect(wrapper.html()).to.match(/hello world/);
+    expect(wrapper.html()).toMatch(/hello world/);
   });
 });
