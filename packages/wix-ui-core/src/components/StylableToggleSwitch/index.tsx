a84ff71e8f73df13db37da63c8864a733260015a
barak igal <baraki@wix.com>
2018-01-08 15:53:46 +0200

    Update index.tsx
    better way to integrate stylable (WIP)

--- packages/wix-ui-core/src/components/StylableToggleSwitch/index.tsx
+++ packages/wix-ui-core/src/components/StylableToggleSwitch/index.tsx
@@ -5,19 +5,27 @@ import style from './ToggleSwitch.st.css';
 // Generic adapter to add the root class and it's css states
 export function withStylable(CoreComponent: React.ComponentClass<any>, stylesheet: RuntimeStylesheet, getState: (object) => StateMap) {
   return class StylableComponent extends CoreComponent {
+    static stylable_stylesheet = stylesheet;
     render() {
       this.props = {...this.props, classes: stylesheet};
       const root = super.render();
-
-      if (!root) {
-        return null;
-      }
-
-      const className = root.props.className && root.props.className.indexOf(stylesheet.root) === -1 ?
-        'root ' + root.props.className :
-        root.props.className;
-
-      return React.cloneElement(root, stylesheet(className, getState(this.props), this.props));
+      if (!root) { return null; }
+      const isProcessedByStylable = root.props && root.props["data-stylable"];
+      const states = getState(this.props);
+      const className = root.props && root.props.className || '';
+      const props = isProcessedByStylable ? 
+          stylesheet(
+            className ? 'root ' + className : 'root',
+            states
+          ) : 
+          stylesheet(
+            className, 
+            states,
+            this.props
+          );
+            
+      props["data-stylable"] = true;
+      return React.cloneElement(root, props);
     }
   };
 }
