f488cc9332d81ffe8e3860d7029691925ff5ef45
Tom Enden <tome@wix.com>
2018-01-08 11:41:52 +0200

    wix-ui-test-utils: support enzyme 3 (#152)

--- packages/wix-ui-test-utils/src/testkit-helpers/enzyme.ts
+++ packages/wix-ui-test-utils/src/testkit-helpers/enzyme.ts
@@ -5,8 +5,9 @@ import {reactEventTrigger} from '../helpers';
 export const enzymeTestkitFactoryCreator = driverFactory => ({wrapper, dataHook}) => {
   const eventTrigger = reactEventTrigger();
   const regexp = new RegExp(`^<[^>]+data-hook="${dataHook}"`);
-  const component = wrapper.findWhere(n => typeof n.type() === 'string' && (regexp).test(n.html()));
-  return driverFactory({element: component.node, wrapper, eventTrigger});
+  const component = wrapper.findWhere(n => n.length > 0 && typeof n.type() === 'string' && (regexp).test(n.html()));
+  const element = component.length > 0 ? component.first().getDOMNode() : undefined;
+  return driverFactory({element, wrapper, eventTrigger});
 };
 
 export const isEnzymeTestkitExists = (Element, testkitFactory, options = {withoutDataHook: false}) => {
