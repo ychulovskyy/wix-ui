f00a38e52560520cc5267dc6fdffaabd1afc002f
Ronen Stern <stern.ronen@gmail.com>
2018-01-11 20:29:09 +0200

    wix-ui-icons-common: Fix icons not exported to npm (#159)
    * wix-ui-icons-common: Fix icons not exported to npm
    
    * Exclude some root scripts
    
    * Fix system export paths and index file
    
    * minor improvements to build phase
    
    * Update package.json

--- packages/wix-ui-icons-common/scripts/create-aliases.js
+++ packages/wix-ui-icons-common/scripts/create-aliases.js
@@ -1,5 +1,6 @@
+const path = require('path');
 const fse = require('fs-extra');
-const [from, to] = process.argv.slice(2);
+const [from, to, indexPath] = process.argv.slice(2);
 
 if (to && to !== '.' ) {
   fse.removeSync(to);
@@ -7,4 +8,14 @@ if (to && to !== '.' ) {
 }
 fse.readdirSync(from)
   .filter(file => file.endsWith('.js'))
-  .forEach(file => fse.writeFileSync(`${to}/${file}`, `module.exports = require('${from}/${file}');\n`));
+  .forEach(file => fse.writeFileSync(
+    `${to}/${file}`,
+    `module.exports = require('${path.relative(to, from)}/${file}');\n`
+  ));
+
+if (indexPath) {
+  fse.writeFileSync(
+    `${to}/index.js`,
+    `module.exports = require('${path.relative(to, indexPath)}');\n`
+  )
+}
