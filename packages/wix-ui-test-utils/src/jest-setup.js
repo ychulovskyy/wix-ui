8eefb210f3b6231bb969fb4e1ebe2ff33df93339
Amir Yonatan <amiry@wix.com>
2018-01-12 17:10:18 +0200

    Fixed enzyme imports

--- packages/wix-ui-test-utils/src/jest-setup.js
+++ packages/wix-ui-test-utils/src/jest-setup.js
@@ -1,7 +1,7 @@
-const Enzyme = require('enzyme');
-const Adapter = require('enzyme-adapter-react-15');
+import {configure} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-15';
 
-Enzyme.configure({adapter: new Adapter()});
+configure({ adapter: new Adapter() });
 
 const originConsoleError = console.error;
 let errors = [];
