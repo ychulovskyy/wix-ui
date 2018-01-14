c677444244ceff7fd6bbaf4e4311936624dd8dad
Eran Shabi <eran514@gmail.com>
2018-01-14 11:57:37 +0200

    wix-ui-test-utils: remove redundant imports (#165)

--- packages/wix-ui-test-utils/src/jest-setup.js
+++ packages/wix-ui-test-utils/src/jest-setup.js
@@ -1,8 +1,3 @@
-import {configure} from 'enzyme';
-import Adapter from 'enzyme-adapter-react-15';
-
-configure({ adapter: new Adapter() });
-
 const originConsoleError = console.error;
 let errors = [];
 
