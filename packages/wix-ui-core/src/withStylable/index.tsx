9c005d6c59c8ed1e12712e2fede07c60680e8d1a
Amir Yonatan <amiry@wix.com>
2018-01-17 17:13:43 +0200

    Adding custom interface

--- packages/wix-ui-core/src/withStylable/index.tsx
+++ packages/wix-ui-core/src/withStylable/index.tsx
@@ -1,8 +1,18 @@
 
 import * as React from 'react';
+
+export interface StateComponentClass<P = {}, S = {}> {
+  new (props?: P, context?: any): React.Component<P, S>;
+  propTypes?: React.ValidationMap<P>;
+  contextTypes?: React.ValidationMap<any>;
+  childContextTypes?: React.ValidationMap<any>;
+  defaultProps?: Partial<P>;
+  displayName?: string;
+}
+
 // Generic adapter to add the root class and it's css states
-export function withStylable<T>(
-  CoreComponent: React.ComponentClass<T>,
+export function withStylable<P, S>(
+  CoreComponent: StateComponentClass<P, S>,
   stylesheet: RuntimeStylesheet,
   getState: (p?: any, s?: any, c?: any) => StateMap) {
 
