cde84e29acc34e9097c72eb1c6b2d88a3d8463bb
Shlomi Toussia-Cohen <shlomitc@wix.com>
2018-01-23 19:03:32 +0200

    Revert "Fixed DropdownContent driver"
    This reverts commit df655162b0138a54365df02a24b1aa3e41b70b59.

--- packages/wix-ui-core/src/baseComponents/DropdownContent/DropdownContent.driver.ts
+++ packages/wix-ui-core/src/baseComponents/DropdownContent/DropdownContent.driver.ts
@@ -1,25 +1,13 @@
-import {BaseDriver, ComponentFactory} from 'wix-ui-test-utils';
-import {DropdownContent} from './index';
-
 const getOptionAt = (element, index) => element.querySelectorAll('[data-hook="option"]')[index];
 
-export class DropdownDriver implements BaseDriver {
-  private componentFactory: ComponentFactory;
-  constructor(componentFactory: ComponentFactory) {
-    this.componentFactory = componentFactory;
-  }
-
-  exists = () => !!this.componentFactory.element;
-  onKeyDown = key => (this.componentFactory.componentInstance as DropdownContent).onKeyDown({key} as React.KeyboardEvent<HTMLElement>);
-  optionAt = index => {
-    const option = getOptionAt(this.componentFactory.element, index);
+export const dropdownContentDriverFactory = ({element, componentInstance, eventTrigger}) => ({
+  exists: () => !!element,
+  onKeyDown: key => componentInstance.onKeyDown({key}),
+  optionAt: index => {
+    const option = getOptionAt(element, index);
     return {
-      click: () => this.componentFactory.eventTrigger.click(option),
+      click: () => eventTrigger.click(option),
       containsClass: className => option.className.includes(className)
     };
   }
-}
-
-export const dropdownContentDriverFactory = (componentFactory: ComponentFactory) => {
-  return new DropdownDriver(componentFactory);
-};
+});
--- packages/wix-ui-test-utils/src/createDriverFactory.tsx
+++ packages/wix-ui-test-utils/src/createDriverFactory.tsx
@@ -4,7 +4,7 @@ import {Simulate} from 'react-dom/test-utils';
 import {reactEventTrigger} from './helpers';
 import {ReactWrapper} from 'enzyme';
 
-export type DriverFactory<TDriver extends BaseDriver> = (compFactory: ComponentFactory) => TDriver;
+export type DriverFactory<T> = (compFactory: ComponentFactory) => T;
 
 export interface BaseDriver {
   exists: () => boolean;
@@ -14,7 +14,7 @@ export interface ComponentFactory {
   element: Node | Element | undefined;
   wrapper: HTMLElement | ReactWrapper;
   component?: React.ReactElement<any>;
-  componentInstance?: any;
+  componentInstance?: React.ReactInstance | undefined;
   eventTrigger: typeof Simulate;
 }
 
@@ -24,11 +24,11 @@ const componentFactory: (Component: React.ReactElement<any>) => ComponentFactory
   const eventTrigger = reactEventTrigger();
 
   const wrapperDiv = document.createElement('div');
-  const ClonedComponent = React.cloneElement(Component, {ref: (r: any) => componentInstance = r});
+  const ClonedComponent = React.cloneElement(Component, {ref: (r: Element) => componentInstance = r});
   render(<div ref={r => element = r}>{ClonedComponent}</div>, wrapperDiv);
   return {element: element! && element!.childNodes[0], wrapper: wrapperDiv, component: ClonedComponent, componentInstance, eventTrigger};
 };
 
-export function createDriverFactory<TDriver extends BaseDriver>(driverFactory: DriverFactory<TDriver>) {
+export function createDriverFactory<T>(driverFactory: DriverFactory<T>) {
   return (Component: React.ReactElement<any>) => driverFactory(componentFactory(Component));
 }
