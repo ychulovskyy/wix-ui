e6cd5baf51c6c46d82a3a7043447a4fd9d172dd5
Kieran Williams <kieranw@wix.com>
2018-01-24 17:41:04 +0200

    Removed unused import

--- packages/wix-ui-core/test/enzyme-setup.js
+++ packages/wix-ui-core/test/enzyme-setup.js
@@ -1,5 +1,4 @@
 const Enzyme = require('enzyme');
 const Adapter = require('enzyme-adapter-react-15');
-const stylable = require('stylable-integration/require');
 
 Enzyme.configure({adapter: new Adapter()});
