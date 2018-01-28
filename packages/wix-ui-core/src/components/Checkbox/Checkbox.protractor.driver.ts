63e06e3fc11899dda7390448d018876c96040197
Kieran Williams <kieranw@wix.com>
2018-01-28 11:02:11 +0200

    Added comments to Protractor driver

--- packages/wix-ui-core/src/components/Checkbox/Checkbox.protractor.driver.ts
+++ packages/wix-ui-core/src/components/Checkbox/Checkbox.protractor.driver.ts
@@ -1,5 +1,8 @@
 export const checkboxDriverFactory = component => ({
+  /** Returns the component instance */
   element: () => component,
+  /** Simulates a click on the component */
   click: () => component.click(),
+  /** Indicates whether the component is disabled or not */
   isDisabled: () => !!component.getAttribute('disabled')
 });
