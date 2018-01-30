40c05c7235d103dd832f0494dadcfa8fe296cf12
Kieran Williams <kieranw@wix.com>
2018-01-30 09:28:52 +0200

    Changed data-hook (#204)

--- packages/wix-ui-core/src/components/Checkbox/Checkbox.e2e.ts
+++ packages/wix-ui-core/src/components/Checkbox/Checkbox.e2e.ts
@@ -3,13 +3,13 @@ import {browser} from 'protractor';
 import {getStoryUrl, waitForVisibilityOf} from 'wix-ui-test-utils';
 import {checkboxTestkitFactory} from '../../testkit/protractor';
 
-xdescribe('Checkbox', () => {
+describe('Checkbox', () => {
   const storyUrl = getStoryUrl('Components', 'Checkbox');
 
   beforeEach(() => browser.get(storyUrl));
 
   eyes.it('should display correct content', () => {
-    const dataHook = 'story-checkbox';
+    const dataHook = 'storybook-checkbox';
     const driver = checkboxTestkitFactory({dataHook});
 
     return waitForVisibilityOf(driver.element(), 'Cannot find Checkbox');
