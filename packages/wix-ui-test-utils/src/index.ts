19cc0d35e790dc3baa26a1f00e1a2ad04c358254
Amir Yonatan <amiry@wix.com>
2018-02-07 17:23:51 +0200

    Added index.ts again for backward compatability

---
+++ packages/wix-ui-test-utils/src/index.ts
@@ -0,0 +1,12 @@
+// DO NOT USE THIS FILE!
+// This is file is deprecated, do not import from it.
+// It's used for backward compatability in wix-style-react 1.*
+
+export {createDriverFactory, DriverFactory, BaseDriver, ComponentFactory} from './driver-factory';
+export {isClassExists, makeControlled, sleep} from './react-helpers';
+export {getStoryUrl, scrollToElement, waitForVisibilityOf, protractorTestkitFactoryCreator} from './protractor';
+
+export {testkitFactoryCreator, isTestkitExists, isAttributeExists} from './vanilla';
+export {enzymeTestkitFactoryCreator, isEnzymeTestkitExists, WrapperData} from './enzyme';
+export {puppeteerTestkitFactoryCreator} from './puppeteer';
+export {baseProtractorConfig} from './protractor';
