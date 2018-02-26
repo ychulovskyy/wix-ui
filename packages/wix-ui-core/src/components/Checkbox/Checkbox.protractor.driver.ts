0512bf5565a99cb29351ad7b23499c7ad3ef4096
danielstro <danielstr@wix.com>
2018-02-26 16:00:30 +0200

    <Checkbox /> Fix protractor driver (#272)
    * initial commit
    
    * remove duplicated test
    
    * beautified code and removed async

--- packages/wix-ui-core/src/components/Checkbox/Checkbox.protractor.driver.ts
+++ packages/wix-ui-core/src/components/Checkbox/Checkbox.protractor.driver.ts
@@ -1,10 +1,10 @@
 export const checkboxDriverFactory = component => ({
-  /** Returns the component instance */
-  element: () => component,
-  /** Simulates a click on the component */
-  click: () => component.click(),
-  /** Indicates whether the component is disabled or not */
-  isDisabled: () => component.getAttribute('disabled') === '',
-  /** Indicates whether the component is checked */
-  isChecked: () => component.$('[data-hook="CHECKBOX_TICKMARK"]').isPresent()
-});
+    /** Returns the component instance */
+    element: () => component,
+    /** Simulates a click on the component */
+    click: () => component.click(),
+    /** Indicates whether the component is disabled or not */
+    isDisabled: () => component.getAttribute('disabled') === '',
+    /** Indicates whether the component is checked */
+    isChecked: () => component.getAttribute('aria-checked') !== 'false'
+  });
