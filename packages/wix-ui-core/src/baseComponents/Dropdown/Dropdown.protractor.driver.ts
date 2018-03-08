cd5271560990f3135dbcec3e9b716cb5cf9ed837
Amir Yonatan <amiry@wix.com>
2018-03-08 17:57:26 +0200

    added driver changes

--- packages/wix-ui-core/src/baseComponents/Dropdown/Dropdown.protractor.driver.ts
+++ packages/wix-ui-core/src/baseComponents/Dropdown/Dropdown.protractor.driver.ts
@@ -1,11 +1,12 @@
 import {dropdownContentDriverFactory} from '../DropdownContent/DropdownContent.protractor.driver';
+import {ElementFinder} from 'protractor';
 
 export const dropdownDriverFactory = component => {
-  const getDropdownContentDriver = () => dropdownContentDriverFactory(component.$('[data-hook="popover-content"] > div'));
+  const getDropdownContentDriver = () => dropdownContentDriverFactory(component.$('[data-hook="popover-content"]'));
 
   return {
     element: () => component,
-    isOpen: () => !!getDropdownContentDriver().element,
+    isOpen: () => getDropdownContentDriver().element().isPresent(),
     getOptionsCount: () => getDropdownContentDriver().getOptionsCount(),
     selectOption: (index: number) => getDropdownContentDriver().selectOption(index),
     getOptionText: (index: number) => getDropdownContentDriver().getOptionText(index)
