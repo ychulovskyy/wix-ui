f35b02a4c99f07baa6bf6e53b89bd9d254027990
Amir Yonatan <amiry@wix.com>
2018-03-12 15:10:57 +0200

    Added Highlighter stylable

--- packages/wix-ui-core/src/baseComponents/Highlighter/Highlighter.spec.tsx
+++ packages/wix-ui-core/src/baseComponents/Highlighter/Highlighter.spec.tsx
@@ -6,7 +6,7 @@ describe('Highlighter', () => {
   it('should wrap children with strong tag', () => {
     const wrapper = mount(<Highlighter>Text</Highlighter>);
     const element = wrapper.children().at(0).getDOMNode();
-    expect(element.tagName).toEqual('STRONG');
+    expect(element.tagName).toEqual('SPAN');
     expect(element.innerHTML).toEqual('Text');
   });
 });
