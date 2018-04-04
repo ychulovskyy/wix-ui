b9a2fa7d6c9db65c956a773fbfa70b18251ede74
Jonathan Marton <jonathan.marton@gmail.com>
2018-04-04 16:33:43 +0300

    Pagi- adding minHeight along to minWidth (#376)

--- packages/wix-ui-core/src/components/Pagination/root-min-width.ts
+++ packages/wix-ui-core/src/components/Pagination/root-min-width.ts
@@ -2,17 +2,26 @@ import {getId} from './Pagination';
 
 export const measureAndSetRootMinWidth = (compNode: HTMLElement, paginationMode: 'pages' | 'input', idPrefix: string = ''): void => {
     compNode.style.minWidth = '';
+    compNode.style.minHeight = '';
 
     const getById = (id: string): HTMLElement => compNode.querySelector(`#${getId(idPrefix, id)}`);
 
     const getMinWidth = (elmnt: HTMLElement): number => elmnt ? parseInt(window.getComputedStyle(elmnt).minWidth, 10) : 0;
 
-    const getWidthWithMargins = (elmnt: HTMLElement): number => {
-        if (!elmnt) {return 0; }
-        const style = window.getComputedStyle(elmnt);
+    const getWidthWithMargins = (element: HTMLElement): number => {
+        if (!element) {return 0; }
+        const style = window.getComputedStyle(element);
         return parseInt(style.marginRight, 10) +
-        elmnt.offsetWidth +
+        element.offsetWidth +
         parseInt(style.marginLeft, 10);
+    };
+
+    const getHeightWithMargins = (element: HTMLElement): number => {
+        if (!element) {return 0; }
+        const style = window.getComputedStyle(element);
+        return parseInt(style.marginBottom, 10) +
+        element.offsetHeight +
+        parseInt(style.marginTop, 10);
     };
 
     const navButtonsMinWidth = getWidthWithMargins(getById('navButtonNext')) +
@@ -21,16 +30,22 @@ export const measureAndSetRootMinWidth = (compNode: HTMLElement, paginationMode:
     getWidthWithMargins(getById('navButtonLast'));
 
     let selectionMinWidth = 0;
+    let minHeight = 0;
 
     if (paginationMode === 'pages') {
         // means we're in "pages" pagination mode
         selectionMinWidth = getMinWidth(getById('pageStrip'));
+
+        minHeight = Math.max(getHeightWithMargins(getById('pageStrip')), getHeightWithMargins(getById('navButtonNext')));
     } else {
         // means we're in "input" pagination mode
         selectionMinWidth = getWidthWithMargins(getById('totalPages')) +
         getWidthWithMargins(getById('slash')) +
         getWidthWithMargins(getById('pageInput'));
+
+        minHeight = Math.max(getHeightWithMargins(getById('pageInput')), getHeightWithMargins(getById('navButtonNext')));
     }
 
     compNode.style.minWidth = navButtonsMinWidth + selectionMinWidth + 'px';
+    compNode.style.minHeight = minHeight + 'px';
 };
