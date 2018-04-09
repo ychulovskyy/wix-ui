47adb20be54b3edf207ea6377a767c7703e3c5aa
Alexey Lebedev <alisey@users.noreply.github.com>
2018-04-09 16:49:52 +0300

    Added EmptyTrash icon (#398)

--- packages/wix-ui-icons-common/.storybook/config.js
+++ packages/wix-ui-icons-common/.storybook/config.js
@@ -2,7 +2,7 @@ import {configure} from '@storybook/react';
 import {setOptions} from '@storybook/addon-options';
 
 setOptions({
-  showDownPanel: false,
+  showAddonPanel: false,
   name: 'wix-ui-icons-common',
   url: 'https://github.com/wix/wix-ui/tree/master/packages/wix-ui-icons-common'
 });
