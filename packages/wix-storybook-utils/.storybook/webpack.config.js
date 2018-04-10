ee1f843532a4cf30afa25dcfbd0ddb1146b90b93
argshook <argshook@gmail.com>
2018-04-10 12:43:16 +0300

    docs(storybook/webpack.config): use defaultConfig instead of imported config
    importing is discouraged by storybook

--- packages/wix-storybook-utils/.storybook/webpack.config.js
+++ packages/wix-storybook-utils/.storybook/webpack.config.js
@@ -1,9 +1,8 @@
 const path = require('path');
-const genDefaultConfig = require('@storybook/react/dist/server/config/defaults/webpack.config.js');
 const wixStorybookConfig = require('yoshi/config/webpack.config.storybook');
 
-module.exports = (config, env) => {
-  const newConfig = wixStorybookConfig(genDefaultConfig(config, env));
+module.exports = (config, env, defaultConfig) => {
+  const newConfig = wixStorybookConfig(defaultConfig);
 
   newConfig.resolve.alias = Object.assign({}, newConfig.resolve.alias, {
     'wix-storybook-utils': path.resolve(__dirname, '..', 'src')
