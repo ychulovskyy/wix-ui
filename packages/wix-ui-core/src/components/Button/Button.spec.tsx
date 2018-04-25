8e7666131b457c5da9996540c7fa3edeb9498981
Erez Makavy <erezm@wix.com>
2018-04-25 15:46:00 +0300

    `<Button>` - pass rest of props to native button (#440)
    * Button - pass rest of props to native button
    
    * Rename `common/BaseProps.ts` to `types/BaseProps.ts`
    
    * Remove mouseEnter mouseLeave driver methods
    
    * spread all props

--- packages/wix-ui-core/src/components/Button/Button.spec.tsx
+++ packages/wix-ui-core/src/components/Button/Button.spec.tsx
@@ -1,12 +1,10 @@
 import * as React from 'react';
-import {mount} from 'enzyme';
-import {isEnzymeTestkitExists} from 'wix-ui-test-utils/enzyme';
 import {createDriverFactory} from 'wix-ui-test-utils/driver-factory';
-import {isTestkitExists} from 'wix-ui-test-utils/vanilla';
 import {buttonDriverFactory} from './Button.driver';
 import {buttonTestkitFactory} from '../../testkit';
 import {buttonTestkitFactory as enzymeButtonTestkitFactory} from '../../testkit/enzyme';
 import {Button} from './';
+import {runTestkitExistsSuite} from '../../common/testkitTests';
 
 describe('Button', () => {
 
@@ -26,24 +24,6 @@ describe('Button', () => {
       const driver = createDriver(<Button onClick={onClick}/>);
       driver.click();
       expect(onClick).toBeCalled();
-    });
-  });
-
-  describe('onMouseEnter prop', () => {
-    it('should be called on mouse enter', () => {
-      const onMouseEnter = jest.fn();
-      const driver = createDriver(<Button onMouseEnter={onMouseEnter}/>);
-      driver.mouseEnter();
-      expect(onMouseEnter).toBeCalled();
-    });
-  });
-
-  describe('onMouseLeave prop', () => {
-    it('should be called on mouse leave', () => {
-      const onMouseLeave = jest.fn();
-      const driver = createDriver(<Button onMouseLeave={onMouseLeave}/>);
-      driver.mouseLeave();
-      expect(onMouseLeave).toBeCalled();
     });
   });
 
@@ -70,15 +50,10 @@ describe('Button', () => {
     });
   });
 
-  describe('testkit', () => {
-    it('should exist', () => {
-      expect(isTestkitExists(<Button/>, buttonTestkitFactory)).toBe(true);
-    });
+  runTestkitExistsSuite({
+    Element: <Button/>,
+    testkitFactory: buttonTestkitFactory,
+    enzymeTestkitFactory: enzymeButtonTestkitFactory
   });
 
-  describe('enzyme testkit', () => {
-    it('should exist', () => {
-      expect(isEnzymeTestkitExists(<Button/>, enzymeButtonTestkitFactory, mount)).toBe(true);
-    });
-  });
 });
