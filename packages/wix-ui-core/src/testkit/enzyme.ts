29c54b99e533fbb241ee4d4fd2c922f4d1ff23cc
Erez Makavy <erezm@wix.com>
2018-05-06 10:19:12 +0300

    Add BadgeDriver

--- packages/wix-ui-core/src/testkit/enzyme.ts
+++ packages/wix-ui-core/src/testkit/enzyme.ts
@@ -1,68 +1,123 @@
-import {enzymeTestkitFactoryCreator, WrapperData} from 'wix-ui-test-utils/enzyme';
+import {
+  enzymeTestkitFactoryCreator,
+  WrapperData
+} from "wix-ui-test-utils/enzyme";
 
-import {inputDriverFactory} from '../components/Input/Input.driver';
+import { inputDriverFactory } from "../components/Input/Input.driver";
 //Ugly workaround for now
-export const inputTestkitFactory: (obj: WrapperData) => any = enzymeTestkitFactoryCreator(inputDriverFactory);
+export const inputTestkitFactory: (
+  obj: WrapperData
+) => any = enzymeTestkitFactoryCreator(inputDriverFactory);
 
-import {textDriverFactory} from '../components/Text/Text.driver';
-export const textTestkitFactory = enzymeTestkitFactoryCreator(textDriverFactory);
+import { textDriverFactory } from "../components/Text/Text.driver";
+export const textTestkitFactory = enzymeTestkitFactoryCreator(
+  textDriverFactory
+);
 
-import {badgeDriverFactory} from '../components/Badge/Badge.driver';
-export const badgeTestkitFactory = enzymeTestkitFactoryCreator(badgeDriverFactory);
+import { badgeDriverFactory } from "../components/Badge/Badge.driver";
+export const badgeTestkitFactory = enzymeTestkitFactoryCreator(
+  badgeDriverFactory
+);
 
-import {tooltipDriverFactory} from '../components/Tooltip/Tooltip.driver';
-export const tooltipTestkitFactory = enzymeTestkitFactoryCreator(tooltipDriverFactory);
+import { tooltipDriverFactory } from "../components/Tooltip/Tooltip.driver";
+export const tooltipTestkitFactory = enzymeTestkitFactoryCreator(
+  tooltipDriverFactory
+);
 
-import {paginationDriverFactory} from '../components/Pagination/Pagination.driver';
-export const paginationTestkitFactory = enzymeTestkitFactoryCreator(paginationDriverFactory);
+import { paginationDriverFactory } from "../components/Pagination/Pagination.driver";
+export const paginationTestkitFactory = enzymeTestkitFactoryCreator(
+  paginationDriverFactory
+);
 
-import {dividerDriverFactory} from '../components/Divider/Divider.driver';
-export const dividerTestkitFactory = enzymeTestkitFactoryCreator(dividerDriverFactory);
+import { dividerDriverFactory } from "../components/Divider/Divider.driver";
+export const dividerTestkitFactory = enzymeTestkitFactoryCreator(
+  dividerDriverFactory
+);
 
-import {checkboxDriverFactory} from '../components/Checkbox/Checkbox.driver';
-export const checkboxTestkitFactory = enzymeTestkitFactoryCreator(checkboxDriverFactory);
+import { checkboxDriverFactory } from "../components/Checkbox/Checkbox.driver";
+export const checkboxTestkitFactory = enzymeTestkitFactoryCreator(
+  checkboxDriverFactory
+);
 
 // Stylable
-import {toggleSwitchDriverFactory} from '../components/ToggleSwitch/ToggleSwitch.driver';
-export const toggleSwitchTestkitFactory = enzymeTestkitFactoryCreator(toggleSwitchDriverFactory);
+import { toggleSwitchDriverFactory } from "../components/ToggleSwitch/ToggleSwitch.driver";
+export const toggleSwitchTestkitFactory = enzymeTestkitFactoryCreator(
+  toggleSwitchDriverFactory
+);
 
-import {textDriverFactory as stylableTextDriverFactory, TextDriver} from '../components/StylableText/Text.driver';
-export const stylableTextTestkitFactory = enzymeTestkitFactoryCreator<TextDriver>(stylableTextDriverFactory);
-export {TextDriver};
+import {
+  textDriverFactory as stylableTextDriverFactory,
+  TextDriver
+} from "../components/StylableText/Text.driver";
+export const stylableTextTestkitFactory = enzymeTestkitFactoryCreator<
+  TextDriver
+>(stylableTextDriverFactory);
+export { TextDriver };
 
-import {buttonDriverFactory} from '../components/Button/Button.driver';
-export const buttonTestkitFactory = enzymeTestkitFactoryCreator(buttonDriverFactory);
+import { buttonDriverFactory } from "../components/Button/Button.driver";
+export const buttonTestkitFactory = enzymeTestkitFactoryCreator(
+  buttonDriverFactory
+);
 
-import {badgeDriverFactory as stylableBadgeDriverFactory} from '../components/StylableBadge/Badge.driver';
-export const stylableBadgeTestkitFactory = enzymeTestkitFactoryCreator(stylableBadgeDriverFactory);
+import {
+  badgeDriverFactory as stylableBadgeDriverFactory,
+  BadgeDriver as StylableBadgeDriver
+} from "../components/StylableBadge/Badge.driver";
+export const stylableBadgeTestkitFactory = enzymeTestkitFactoryCreator<
+  StylableBadgeDriver
+>(stylableBadgeDriverFactory);
+export { StylableBadgeDriver };
 
-import {radioButtonDriverFactory} from '../components/RadioButton/RadioButton.driver';
-export const radioButtonTestkitFactory = enzymeTestkitFactoryCreator(radioButtonDriverFactory);
+import { radioButtonDriverFactory } from "../components/RadioButton/RadioButton.driver";
+export const radioButtonTestkitFactory = enzymeTestkitFactoryCreator(
+  radioButtonDriverFactory
+);
 
-import {autocompleteDriverFactory} from '../components/Autocomplete/Autocomplete.driver';
-export const autocompleteTestkitFactory = enzymeTestkitFactoryCreator(autocompleteDriverFactory);
+import { autocompleteDriverFactory } from "../components/Autocomplete/Autocomplete.driver";
+export const autocompleteTestkitFactory = enzymeTestkitFactoryCreator(
+  autocompleteDriverFactory
+);
 
-import {sliderDriverFactory} from '../components/Slider/Slider.driver';
-export const sliderTestkitFactory = enzymeTestkitFactoryCreator(sliderDriverFactory);
+import { sliderDriverFactory } from "../components/Slider/Slider.driver";
+export const sliderTestkitFactory = enzymeTestkitFactoryCreator(
+  sliderDriverFactory
+);
 
-import {addressInputDriverFactory} from '../components/AddressInput/AddressInput.driver';
-export const addressInputTestkitFactory = enzymeTestkitFactoryCreator(addressInputDriverFactory);
+import { addressInputDriverFactory } from "../components/AddressInput/AddressInput.driver";
+export const addressInputTestkitFactory = enzymeTestkitFactoryCreator(
+  addressInputDriverFactory
+);
 
-import {labelDriverFactory} from '../components/Label/Label.driver';
-export const labelTestkitFactory = enzymeTestkitFactoryCreator(labelDriverFactory);
+import { labelDriverFactory } from "../components/Label/Label.driver";
+export const labelTestkitFactory = enzymeTestkitFactoryCreator(
+  labelDriverFactory
+);
 
-import {timePickerDriverFactory} from '../components/TimePicker/TimePicker.driver';
-export const timePickerTestkitFactory = enzymeTestkitFactoryCreator(timePickerDriverFactory);
+import { timePickerDriverFactory } from "../components/TimePicker/TimePicker.driver";
+export const timePickerTestkitFactory = enzymeTestkitFactoryCreator(
+  timePickerDriverFactory
+);
 
-import {labelWithOptionsDriverFactory} from '../components/LabelWithOptions/LabelWithOptions.driver';
-export const labelWithOptionsTestkitFactory = enzymeTestkitFactoryCreator(labelWithOptionsDriverFactory);
+import { labelWithOptionsDriverFactory } from "../components/LabelWithOptions/LabelWithOptions.driver";
+export const labelWithOptionsTestkitFactory = enzymeTestkitFactoryCreator(
+  labelWithOptionsDriverFactory
+);
 
-import {thumbnailDriverFactory, ThumbnailDriver} from '../components/Thumbnail/Thumbnail.driver';
-export const thumbnailTestkitFactory = enzymeTestkitFactoryCreator<ThumbnailDriver>(thumbnailDriverFactory);
-export {ThumbnailDriver};
+import {
+  thumbnailDriverFactory,
+  ThumbnailDriver
+} from "../components/Thumbnail/Thumbnail.driver";
+export const thumbnailTestkitFactory = enzymeTestkitFactoryCreator<
+  ThumbnailDriver
+>(thumbnailDriverFactory);
+export { ThumbnailDriver };
 
-import {popoverDriverFactory} from '../components/Popover/Popover.driver';
-export const popoverTestkitFactory = enzymeTestkitFactoryCreator(popoverDriverFactory);
+import { popoverDriverFactory } from "../components/Popover/Popover.driver";
+export const popoverTestkitFactory = enzymeTestkitFactoryCreator(
+  popoverDriverFactory
+);
 
-import {linkDriverFactory} from '../components/Link/Link.driver';
-export const linkTestkitFactory = enzymeTestkitFactoryCreator(linkDriverFactory);
+import { linkDriverFactory } from "../components/Link/Link.driver";
+export const linkTestkitFactory = enzymeTestkitFactoryCreator(
+  linkDriverFactory
+);
--- packages/wix-ui-core/src/testkit/index.ts
+++ packages/wix-ui-core/src/testkit/index.ts
@@ -1,67 +1,105 @@
-import {testkitFactoryCreator} from 'wix-ui-test-utils/vanilla';
+import { testkitFactoryCreator } from "wix-ui-test-utils/vanilla";
 
-import {inputDriverFactory} from '../components/Input/Input.driver';
+import { inputDriverFactory } from "../components/Input/Input.driver";
 export const inputTestkitFactory = testkitFactoryCreator(inputDriverFactory);
 
-import {textDriverFactory} from '../components/Text/Text.driver';
+import { textDriverFactory } from "../components/Text/Text.driver";
 export const textTestkitFactory = testkitFactoryCreator(textDriverFactory);
 
-import {paginationDriverFactory} from '../components/Pagination/Pagination.driver';
-export const paginationTestkitFactory = testkitFactoryCreator(paginationDriverFactory);
+import { paginationDriverFactory } from "../components/Pagination/Pagination.driver";
+export const paginationTestkitFactory = testkitFactoryCreator(
+  paginationDriverFactory
+);
 
-import {badgeDriverFactory} from '../components/Badge/Badge.driver';
+import { badgeDriverFactory } from "../components/Badge/Badge.driver";
 export const badgeTestkitFactory = testkitFactoryCreator(badgeDriverFactory);
 
-import {tooltipDriverFactory} from '../components/Tooltip/Tooltip.driver';
-export const tooltipTestkitFactory = testkitFactoryCreator(tooltipDriverFactory);
+import { tooltipDriverFactory } from "../components/Tooltip/Tooltip.driver";
+export const tooltipTestkitFactory = testkitFactoryCreator(
+  tooltipDriverFactory
+);
 
-import {dividerDriverFactory} from '../components/Divider/Divider.driver';
-export const dividerTestkitFactory = testkitFactoryCreator(dividerDriverFactory);
+import { dividerDriverFactory } from "../components/Divider/Divider.driver";
+export const dividerTestkitFactory = testkitFactoryCreator(
+  dividerDriverFactory
+);
 
-import {checkboxDriverFactory} from '../components/Checkbox/Checkbox.driver';
-export const checkboxTestkitFactory = testkitFactoryCreator(checkboxDriverFactory);
+import { checkboxDriverFactory } from "../components/Checkbox/Checkbox.driver";
+export const checkboxTestkitFactory = testkitFactoryCreator(
+  checkboxDriverFactory
+);
 
 //Stylable
-import {toggleSwitchDriverFactory} from '../components/ToggleSwitch/ToggleSwitch.driver';
-export const toggleSwitchTestkitFactory = testkitFactoryCreator(toggleSwitchDriverFactory);
+import { toggleSwitchDriverFactory } from "../components/ToggleSwitch/ToggleSwitch.driver";
+export const toggleSwitchTestkitFactory = testkitFactoryCreator(
+  toggleSwitchDriverFactory
+);
 
-import {textDriverFactory as stylableTextDriverFactory, TextDriver} from '../components/StylableText/Text.driver';
-export const stylableTextTestkitFactory = testkitFactoryCreator<TextDriver>(stylableTextDriverFactory);
-export {TextDriver};
+import {
+  textDriverFactory as stylableTextDriverFactory,
+  TextDriver
+} from "../components/StylableText/Text.driver";
+export const stylableTextTestkitFactory = testkitFactoryCreator<TextDriver>(
+  stylableTextDriverFactory
+);
+export { TextDriver };
 
-import {buttonDriverFactory} from '../components/Button/Button.driver';
+import { buttonDriverFactory } from "../components/Button/Button.driver";
 export const buttonTestkitFactory = testkitFactoryCreator(buttonDriverFactory);
 
-import {badgeDriverFactory as stylableBadgeDriverFactory} from '../components/StylableBadge/Badge.driver';
-export const stylableBadgeTestkitFactory = testkitFactoryCreator(stylableBadgeDriverFactory);
+import {
+  badgeDriverFactory as stylableBadgeDriverFactory,
+  BadgeDriver as StylableBadgeDriver
+} from "../components/StylableBadge/Badge.driver";
+export const stylableBadgeTestkitFactory = testkitFactoryCreator<
+  StylableBadgeDriver
+>(stylableBadgeDriverFactory);
+export { StylableBadgeDriver };
 
-import {radioButtonDriverFactory} from '../components/RadioButton/RadioButton.driver';
-export const radioButtonTestkitFactory = testkitFactoryCreator(radioButtonDriverFactory);
+import { radioButtonDriverFactory } from "../components/RadioButton/RadioButton.driver";
+export const radioButtonTestkitFactory = testkitFactoryCreator(
+  radioButtonDriverFactory
+);
 
-import {autocompleteDriverFactory} from '../components/Autocomplete/Autocomplete.driver';
-export const autocompleteTestkitFactory = testkitFactoryCreator(autocompleteDriverFactory);
+import { autocompleteDriverFactory } from "../components/Autocomplete/Autocomplete.driver";
+export const autocompleteTestkitFactory = testkitFactoryCreator(
+  autocompleteDriverFactory
+);
 
-import {sliderDriverFactory} from '../components/Slider/Slider.driver';
+import { sliderDriverFactory } from "../components/Slider/Slider.driver";
 export const sliderTestkitFactory = testkitFactoryCreator(sliderDriverFactory);
 
-import {addressInputDriverFactory} from '../components/AddressInput/AddressInput.driver';
-export const addressInputTestkitFactory = testkitFactoryCreator(addressInputDriverFactory);
+import { addressInputDriverFactory } from "../components/AddressInput/AddressInput.driver";
+export const addressInputTestkitFactory = testkitFactoryCreator(
+  addressInputDriverFactory
+);
 
-import {labelDriverFactory} from '../components/Label/Label.driver';
+import { labelDriverFactory } from "../components/Label/Label.driver";
 export const labelTestkitFactory = testkitFactoryCreator(labelDriverFactory);
 
-import {timePickerDriverFactory} from '../components/TimePicker/TimePicker.driver';
-export const timePickerTestkitFactory = testkitFactoryCreator(timePickerDriverFactory);
+import { timePickerDriverFactory } from "../components/TimePicker/TimePicker.driver";
+export const timePickerTestkitFactory = testkitFactoryCreator(
+  timePickerDriverFactory
+);
 
-import {labelWithOptionsDriverFactory} from '../components/LabelWithOptions/LabelWithOptions.driver';
-export const labelWithOptionsTestkitFactory = testkitFactoryCreator(labelWithOptionsDriverFactory);
+import { labelWithOptionsDriverFactory } from "../components/LabelWithOptions/LabelWithOptions.driver";
+export const labelWithOptionsTestkitFactory = testkitFactoryCreator(
+  labelWithOptionsDriverFactory
+);
 
-import {thumbnailDriverFactory, ThumbnailDriver} from '../components/Thumbnail/Thumbnail.driver';
-export const thumbnailTestkitFactory = testkitFactoryCreator<ThumbnailDriver>(thumbnailDriverFactory);
-export {ThumbnailDriver};
+import {
+  thumbnailDriverFactory,
+  ThumbnailDriver
+} from "../components/Thumbnail/Thumbnail.driver";
+export const thumbnailTestkitFactory = testkitFactoryCreator<ThumbnailDriver>(
+  thumbnailDriverFactory
+);
+export { ThumbnailDriver };
 
-import {popoverDriverFactory} from '../components/Popover/Popover.driver';
-export const popoverTestkitFactory = testkitFactoryCreator(popoverDriverFactory);
+import { popoverDriverFactory } from "../components/Popover/Popover.driver";
+export const popoverTestkitFactory = testkitFactoryCreator(
+  popoverDriverFactory
+);
 
-import {linkDriverFactory} from '../components/Link/Link.driver';
+import { linkDriverFactory } from "../components/Link/Link.driver";
 export const linkTestkitFactory = testkitFactoryCreator(linkDriverFactory);
