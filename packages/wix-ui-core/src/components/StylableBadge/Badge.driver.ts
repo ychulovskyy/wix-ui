b196e1addd866d368a2d50328a1a3588f73a4bd9
Erez Makavy <erezm@wix.com>
2018-05-06 10:30:23 +0300

    Add BadgeDriver (#468)

--- packages/wix-ui-core/src/components/StylableBadge/Badge.driver.ts
+++ packages/wix-ui-core/src/components/StylableBadge/Badge.driver.ts
@@ -1,12 +1,21 @@
-export const badgeDriverFactory = ({element}) => {
-    return {
-      /** checks if element exists */
-      exists: () => !!element,
-      /** returns elements type attribute */
-      getType: () => element.getAttribute('type'),
-      /** returns elements innerHtml */
-      getContent: () => element.innerHTML,
-      /** returns elements innerText */
-      text: () => element.textContent,
-    };
+import { ComponentFactory, BaseDriver } from "wix-ui-test-utils/driver-factory";
+
+export interface BadgeDriver extends BaseDriver {
+  /** returns elements innerHtml */
+  getContent: () => string;
+  /** returns elements innerText */
+  text: () => string;
+}
+
+export const badgeDriverFactory = ({
+  element
+}: ComponentFactory): BadgeDriver => {
+  return {
+    /** checks if element exists */
+    exists: () => !!element,
+    /** returns elements innerHtml */
+    getContent: () => element.innerHTML,
+    /** returns elements innerText */
+    text: () => element.textContent
   };
+};
