231cd843e148af1b2825903eb21ffd87d2a41046
Erez Makavy <erezm@wix.com>
2018-05-06 10:42:48 +0300

    Add ButtonDriver type (#466)

--- packages/wix-ui-core/src/components/Button/Button.driver.ts
+++ packages/wix-ui-core/src/components/Button/Button.driver.ts
@@ -1,27 +1,42 @@
-export const buttonDriverFactory = ({element, eventTrigger}) => {
+import { BaseDriver, ComponentFactory, DriverFactory } from 'wix-ui-test-utils/driver-factory';
+
+export interface ButtonDriver extends BaseDriver {
+  /** click on the button root element */
+  click: () => any;
+  /** returns elements type attribute */
+  getType: () => any;
+  /** returns elements textContent */
+  getTextContent: () => any;
+  /** returns if the element is disabled */
+  isDisabled: () => boolean;
+  styles: {
+    /** returns elements min-width css property */
+    getMinWidth: () => string;
+    /** returns elements width css property */
+    getWidth: () => string;
+    /** returns elements height css property */
+    getHeight: () => string;
+    /** returns elements padding css property */
+    getPadding: () => string;
+    /** returns elements border-radius css property */
+    getBorderRadius: () => string;
+  }
+}
+
+export const buttonDriverFactory: DriverFactory<ButtonDriver> = ({element, eventTrigger}: ComponentFactory): ButtonDriver => {
   const getButtonStyle = () => window.getComputedStyle(element);
 
   return {
-    /** checks if element exists */
     exists: () => !!element,
-    /** click on the element */
     click: () => eventTrigger.click(element),
-    /** returns elements type attribute */
     getType: () => element.getAttribute('type'),
-    /** returns elements textContent */
     getTextContent: () => element.textContent,
-    /** returns if the element is disabled */
     isDisabled: () => element.getAttribute('disabled') === '',
     styles: {
-      /** returns elements min-width css property */
       getMinWidth: () => getButtonStyle().minWidth,
-      /** returns elements width css property */
       getWidth: () => getButtonStyle().width,
-      /** returns elements height css property */
       getHeight: () => getButtonStyle().height,
-      /** returns elements padding css property */
       getPadding: () => getButtonStyle().padding,
-      /** returns elements border-radius css property */
       getBorderRadius: () => getButtonStyle().borderRadius,
     }
   };
