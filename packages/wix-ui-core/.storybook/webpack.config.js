1c848960b32fbe4cd4a7f55d9f1381a253dfd967
argshook <argshook@gmail.com>
2018-05-07 17:31:31 +0300

    wix-ui-core(storybook): update webpack config
    importing file is deprecated

--- packages/wix-ui-core/.storybook/webpack.config.js
+++ packages/wix-ui-core/.storybook/webpack.config.js
@@ -1,8 +1,7 @@
-const genDefaultConfig = require('@storybook/react/dist/server/config/defaults/webpack.config.js');
 const wixStorybookConfig = require('yoshi/config/webpack.config.storybook');
 
-module.exports = (config, env) => {
-  const newConfig = wixStorybookConfig(genDefaultConfig(config, env));
+module.exports = (config, env, defaultConfig) => {
+  const newConfig = wixStorybookConfig(defaultConfig);
 
   newConfig.module.rules.push({
     test: /\.story\.[j|t]sx?$/,
