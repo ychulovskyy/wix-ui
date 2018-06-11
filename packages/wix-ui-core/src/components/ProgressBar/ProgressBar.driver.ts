3281d1cdafe54e27d338076732d2b5adc2fbcfbf
ranz <ranz@wix.com>
2018-06-11 15:53:17 +0300

    icon tests a/c

--- packages/wix-ui-core/src/components/ProgressBar/ProgressBar.driver.ts
+++ packages/wix-ui-core/src/components/ProgressBar/ProgressBar.driver.ts
@@ -1,12 +1,15 @@
 export const progressBarDriverFactory = ({element}) => {
     const getProgressBarForeGroundStyle = () => window.getComputedStyle(element.querySelector('[data-hook="progressbar-foreground"]'));
     const getProgressBarBackgroundStyle = () => window.getComputedStyle(element.querySelector('[data-hook="progressbar-background"]'));
+    const getElement = dataHook => element.querySelector(`[data-hook="${dataHook}"]`)
     
     const driver = {
         exists: () => !!element,
         getWidth: () => getProgressBarForeGroundStyle().width,
         getBackgroundColor: () => getProgressBarBackgroundStyle().background,
-        getForegroundColor: () => getProgressBarForeGroundStyle().background
+        getForegroundColor: () => getProgressBarForeGroundStyle().background,
+        isSuccessIconDisplayed: () => !!getElement('success-icon'),
+        isFailureIconDisplayed: () => !!getElement('failure-icon')
     }
 
     return driver;
--- packages/wix-ui-core/src/components/ProgressBar/ProgressBar.spec.tsx
+++ packages/wix-ui-core/src/components/ProgressBar/ProgressBar.spec.tsx
@@ -22,19 +22,28 @@ describe('ProgressBar', () => {
         expect(driver.getWidth()).toBe(`${defaultProps.value}%`);
     })
 
+    it('should show progress colors with no failure', () => {
+        const driver = createDriver(<ProgressBar {...defaultProps} />);
+        expect(driver.getForegroundColor()).toBe('rgb(0, 0, 0)');
+        expect(driver.getBackgroundColor()).toBe('rgb(227, 228, 227)');
+    })
+
+    it('should show success icon when reaching 100%', () => {
+        const driver = createDriver(<ProgressBar {...{...defaultProps, value: 100}} />);
+        expect(driver.isSuccessIconDisplayed()).toBe(true);
+    })
+
     describe('when error occures', () => {
         let driver;
-        // getBackgroundColor: () => getProgressBarBackgroundStyle().color,
-        // getForegroundColor: () => getProgressBarForeGroundStyle().color
-
+        
         beforeEach(() => {
             const props = {...defaultProps, error: true};
             driver = createDriver(<ProgressBar {...props} />);
         })
         
         it('should show failure colors', () => {
-            expect(driver.getForegroundColor()).toBe('red');
-            expect(driver.getBackgroundColor()).toBe('pink');
+            expect(driver.getForegroundColor()).toBe('rgb(223, 61, 61)');
+            expect(driver.getBackgroundColor()).toBe('rgb(231, 182, 182)');
         })
     })
 
