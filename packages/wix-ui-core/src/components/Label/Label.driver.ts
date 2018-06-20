18648d2882579280553278f0b43aa6d2e7d4ecdd
Lior Belinsky <liorbe@wix.com>
2018-06-20 18:36:07 +0300

    wix-ui-core: fix Label getLabelText function driver (#592)

--- packages/wix-ui-core/src/components/Label/Label.driver.ts
+++ packages/wix-ui-core/src/components/Label/Label.driver.ts
@@ -8,7 +8,7 @@ export const labelDriverFactory = ({element, eventTrigger}) => {
     /** check if element exists */
     exists: () => !!element,
     /** get the label's text */
-    getLabelText: () => element.innerHTML,
+    getLabelText: () => element.textContent,
     /** get the id of the component */
     getId: () => element.getAttribute('id'),
     /** get the "for" attribute of the component */
