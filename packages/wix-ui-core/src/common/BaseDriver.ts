296bb6be57ffc6959a1eb7b3d4415047de29f3b2
Erez Makavy <erez@wix.com>
2018-07-01 10:33:15 +0300

    Extract BaseDriver

---
+++ packages/wix-ui-core/src/common/BaseDriver.ts
@@ -0,0 +1,21 @@
+import { Simulate } from 'react-dom/test-utils';
+
+export interface DriverArgs {
+  element: HTMLElement;
+  eventTrigger: typeof Simulate;
+}
+
+export class BaseDriver {
+  readonly element : HTMLElement;
+  readonly eventTrigger: typeof Simulate;
+
+  constructor({element, eventTrigger}: DriverArgs) {
+    this.element = element;
+    this.eventTrigger = eventTrigger || Simulate;
+  }
+
+  exists = () => !!this.element;
+  mouseEnter = () =>  this.eventTrigger.mouseEnter(this.element);
+  mouseLeave = () =>  this.eventTrigger.mouseLeave(this.element);
+  click = () => this.eventTrigger.click(this.element);
+}
--- packages/wix-ui-core/src/components/Popover/Popover.driver.private.ts
+++ packages/wix-ui-core/src/components/Popover/Popover.driver.private.ts
@@ -1,25 +1,21 @@
-import { Simulate } from 'react-dom/test-utils';
+import { BaseDriver , DriverArgs} from './../../common/BaseDriver';
+
 const queryDocumentOrElement = (element, query) => ((element && element.querySelectorAll(query)[0]) || document && document.querySelector(query));
 const getTargetElement = (element: Element | undefined) => element && element.querySelectorAll('[data-hook="popover-element"]')[0];
 const getContentElement = (element: Element | undefined) => queryDocumentOrElement(element, '[data-hook="popover-content"]');
 const getArrowElement = (element: Element | undefined) => queryDocumentOrElement(element, '[data-hook="popover-arrow"]');
 const getPortalElement = (element: Element | undefined) => queryDocumentOrElement(element, '[data-hook="popover-portal"]');
 
-export class PopoverDriverPrivate {
-  readonly element:HTMLElement;
-  readonly eventTrigger: typeof Simulate;
+export class PopoverDriverPrivate extends BaseDriver {
 
-  constructor({element, eventTrigger}) {
-    this.element = element;
-    this.eventTrigger = eventTrigger || Simulate;
+  constructor(driverArgs: DriverArgs) {
+    super(driverArgs)
   }
 
   getTargetElement = () =>  getTargetElement(this.element);
   getContentElement = () =>  getContentElement(this.element);
   isTargetElementExists = () =>  !!getTargetElement(this.element);
   isContentElementExists = () =>  !!getContentElement(this.element);
-  mouseEnter = () =>  this.eventTrigger.mouseEnter(this.element);
-  mouseLeave = () =>  this.eventTrigger.mouseLeave(this.element);
   inlineStyles = () =>  this.element.style;
   getArrowOffset = () => {
     const {top, left, right, bottom} = (getArrowElement(this.element) as HTMLElement).style;
