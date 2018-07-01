e4a381e30bae18dcf7801c69d5bd35bfe8af06fb
Erez Makavy <erez@wix.com>
2018-07-01 12:52:48 +0300

    Make PopverDriver private driver a symbol member

--- packages/wix-ui-core/src/components/Popover/Popover.driver.ts
+++ packages/wix-ui-core/src/components/Popover/Popover.driver.ts
@@ -1,12 +1,19 @@
 import {PopoverDriverPrivate} from './Popover.driver.private';
 import {BaseDriver, DriverArgs} from '../../common/BaseDriver';
 
+const PRIVATE_DRIVER = Symbol('private driver');
+
 export class PopoverDriver extends BaseDriver {
-  private readonly p: PopoverDriverPrivate;
+
+  private readonly [PRIVATE_DRIVER]: PopoverDriverPrivate;
 
   constructor(driverArgs: DriverArgs) {
     super(driverArgs);
-    this.p = new PopoverDriverPrivate(driverArgs);
+    this[PRIVATE_DRIVER] = new PopoverDriverPrivate(driverArgs);
+  }
+
+  get p() {
+    return this[PRIVATE_DRIVER];
   }
 
   getTargetElement = () => this.p.getTargetElement();
