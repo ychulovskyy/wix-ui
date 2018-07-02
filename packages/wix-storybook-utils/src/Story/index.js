5dd3675dfa21c44678dbb1159000bee69872715d
Erez Makavy <erez@wix.com>
2018-07-02 12:39:01 +0300

    Add exampleId

--- packages/wix-storybook-utils/src/Story/index.js
+++ packages/wix-storybook-utils/src/Story/index.js
@@ -23,31 +23,53 @@ export default ({
     .storiesOf(category, module)
     .add(
       storyName || _metadata.displayName,
-      () =>
-        isE2E ?
-          <div>
-            <AutoExample
-              isInteractive={false}
-              ref={ref => global.autoexample = ref}
-              component={component}
-              componentProps={componentProps}
-              parsedSource={_metadata}
+      () => {
+        if (isE2E) {
+          const exampleIndex = queryString.parse(window.location.search).exampleIndex;
+          const withExamples = queryString.parse(window.location.search).withExamples !== undefined || exampleIndex !== undefined;
+          const shouldDisplayAutoExample = exampleIndex === undefined;
+
+          const renderExamples = () => {
+            if (exampleIndex === undefined) {
+              return examples;
+            }
+
+            if (!Array.isArray(examples)) {
+              throw new Error(`Story E2E: exampleIndex=${exampleIndex} but examples is not an Array`);
+            }
+            return examples[Number(exampleIndex)];
+          };
+
+          return (
+            <div>
+              { shouldDisplayAutoExample &&
+                <AutoExample
+                  isInteractive={false}
+                  ref={ref => global.autoexample = ref}
+                  component={component}
+                  componentProps={componentProps}
+                  parsedSource={_metadata}
+                  />
+              }
+              {withExamples && renderExamples()}
+            </div>
+          );
+        } else {
+          return (
+            <StoryPage
+              {...{
+                component,
+                componentProps,
+                exampleProps,
+                exampleImport,
+                displayName,
+                examples,
+                codeExample,
+                metadata: _metadata,
+                config: _config
+              }}
               />
-
-            {queryString.parse(window.location.search).withExamples !== undefined && examples}
-          </div> :
-
-          <StoryPage
-            {...{
-              component,
-              componentProps,
-              exampleProps,
-              exampleImport,
-              displayName,
-              examples,
-              codeExample,
-              metadata: _metadata,
-              config: _config
-            }}
-            />
+          );
+        }
+      }
       );
