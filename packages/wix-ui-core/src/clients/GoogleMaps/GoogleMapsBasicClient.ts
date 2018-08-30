1d14cfcd8a61a3ef05cb3786b66421f6a52ccc29
Gilad Artzi <gilada@wix.com>
2018-08-30 10:10:43 +0300

    Adding GoogleMapsBasicClient (#709)
    * Adding GoogleMapsBasicClient
    
    * GoogleMapsBasicClient - fixed test

---
+++ packages/wix-ui-core/src/clients/GoogleMaps/GoogleMapsBasicClient.ts
@@ -0,0 +1,91 @@
+import {Omit} from 'type-zoo';
+import {Address, Geocode, MapsClient} from './types';
+
+function defer() {
+  let resolve, reject;
+  const promise = new Promise((_resolve, _reject) => {
+    resolve = _resolve;
+    reject = _reject;
+  });
+  return { promise, resolve, reject };
+}
+
+const locationFuncOrValue = locationProp => {
+  return typeof locationProp === 'function' ? locationProp() : locationProp;
+};
+
+const serializeResult = results =>
+  Object.assign(results, {
+    geometry: {
+      location: {
+        lat: locationFuncOrValue(results.geometry.location.lat),
+        lng: locationFuncOrValue(results.geometry.location.lng),
+      },
+    },
+  });
+
+// placeDetails is not required at the moment
+export class GoogleMapsBasicClient implements Omit<MapsClient, 'placeDetails'> {
+  _autocomplete;
+  _geocoder;
+
+  loadScript(clientId, lang) {
+    if ((window as any).google && (window as any).google.maps) {
+      return;
+    }
+
+    const { promise, resolve } = defer();
+
+    (window as any).initMap = () => {
+      this._autocomplete = new (window as any).google.maps.places.AutocompleteService();
+      this._geocoder = new (window as any).google.maps.Geocoder();
+      resolve();
+    };
+
+    const script = document.createElement('script');
+    script.src = `//maps.googleapis.com/maps/api/js?libraries=places&client=${clientId}&callback=initMap&language=${lang}`;
+    document.body.appendChild(script);
+
+    return promise;
+  }
+
+  useClientId() {
+    return null;
+  }
+
+  async autocomplete(clientId: string, lang: string, request: any): Promise<Array<Address>> {
+    const { promise, resolve, reject } = defer();
+
+    await this.loadScript(clientId, lang);
+    this._autocomplete.getPlacePredictions(
+      typeof request === 'string' ? { input: request } : request,
+      (results, status) => {
+        if (status === 'OK') {
+          resolve(results);
+        } else {
+          reject('ERROR');
+        }
+      },
+    );
+
+    return (promise as Promise<Array<Address>>);
+  }
+
+  async geocode(clientId: string, lang: string, request: any): Promise<Array<Geocode>> {
+    const { promise, resolve, reject } = defer();
+
+    await this.loadScript(clientId, lang);
+    this._geocoder.geocode(
+      typeof request === 'string' ? { address: request } : request,
+      (results, status) => {
+        if (status === 'OK') {
+          resolve(results.map(result => serializeResult(result)));
+        } else {
+          reject('ERROR');
+        }
+      },
+    );
+
+    return (promise as Promise<Array<Geocode>>);
+  }
+}
