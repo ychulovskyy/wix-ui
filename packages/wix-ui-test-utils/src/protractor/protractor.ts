d98d511275e5826ebadb38c89e216fe8e97b273c
Erez Makavy <erez@wix.com>
2018-09-17 09:57:17 -0700

    Fix protractorTestkitFactoryCreator

--- packages/wix-ui-test-utils/src/protractor/protractor.ts
+++ packages/wix-ui-test-utils/src/protractor/protractor.ts
@@ -2,5 +2,5 @@ import {$, ElementFinder} from 'protractor';
 
 export function protractorTestkitFactoryCreator<T> (driverFactory: (e: ElementFinder) => T) {
   return (obj: {dataHook: string, wrapper?: ElementFinder}) => 
-    wrapper ? driverFactory(wrapper.$(`[data-hook='${obj.dataHook}']`)) : driverFactory($(`[data-hook='${obj.dataHook}']`));
+    obj.wrapper ? driverFactory(obj.wrapper.$(`[data-hook='${obj.dataHook}']`)) : driverFactory($(`[data-hook='${obj.dataHook}']`));
 }
