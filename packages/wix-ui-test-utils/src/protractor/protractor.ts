88d8573e98a5cf414366a8e727b119f91b03fca6
Lior Belinsky <liorbe@wix.com>
2018-09-17 22:34:28 +0300

    fix lint

--- packages/wix-ui-test-utils/src/protractor/protractor.ts
+++ packages/wix-ui-test-utils/src/protractor/protractor.ts
@@ -1,6 +1,6 @@
 import {$, ElementFinder} from 'protractor';
 
 export function protractorTestkitFactoryCreator<T> (driverFactory: (e: ElementFinder) => T) {
-  return (obj: {dataHook: string, wrapper?: ElementFinder}) => 
+  return (obj: {dataHook: string, wrapper?: ElementFinder}) =>
     obj.wrapper ? driverFactory(obj.wrapper.$(`[data-hook='${obj.dataHook}']`)) : driverFactory($(`[data-hook='${obj.dataHook}']`));
 }
