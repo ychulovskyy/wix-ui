19d1b903770eb408569eb5fa031dc94fdf64106d
Chen Roth <36155213+ChenRoth@users.noreply.github.com>
2018-09-17 19:43:24 +0300

    optional wrapper in protractor testkit factory (#719)

--- packages/wix-ui-test-utils/src/protractor/protractor.ts
+++ packages/wix-ui-test-utils/src/protractor/protractor.ts
@@ -1,5 +1,6 @@
 import {$, ElementFinder} from 'protractor';
 
 export function protractorTestkitFactoryCreator<T> (driverFactory: (e: ElementFinder) => T) {
-  return (obj: {dataHook: string}) => driverFactory($(`[data-hook='${obj.dataHook}']`));
+  return (obj: {dataHook: string, wrapper?: ElementFinder}) => 
+    wrapper ? driverFactory(wrapper.$(`[data-hook='${obj.dataHook}']`)) : driverFactory($(`[data-hook='${obj.dataHook}']`));
 }
