e4e59971a11803f7e48e8e54b13c5c1d769205c1
Mykolas Sindeikis <mykolass@wix.com>
2018-09-20 09:51:47 +0300

    WIP - UniDriver. Protractor uni driver factory creator + adapter. (#734)
    * Protractor adapter initial
    
    * Adding undriver dependecy

--- packages/wix-ui-test-utils/src/protractor/protractor.ts
+++ packages/wix-ui-test-utils/src/protractor/protractor.ts
@@ -1,6 +1,23 @@
 import {$, ElementFinder} from 'protractor';
+import {protractorUniDriver} from './protractor-adapter';
+import {UniDriver} from 'unidriver';
 
-export function protractorTestkitFactoryCreator<T> (driverFactory: (e: ElementFinder) => T) {
-  return (obj: {dataHook: string, wrapper?: ElementFinder}) =>
-    obj.wrapper ? driverFactory(obj.wrapper.$(`[data-hook='${obj.dataHook}']`)) : driverFactory($(`[data-hook='${obj.dataHook}']`));
+export function protractorTestkitFactoryCreator<T>(
+  driverFactory: (e: ElementFinder) => T
+) {
+  return (obj: { dataHook: string; wrapper?: ElementFinder }) =>
+    obj.wrapper
+      ? driverFactory(obj.wrapper.$(`[data-hook='${obj.dataHook}']`))
+      : driverFactory($(`[data-hook='${obj.dataHook}']`));
+}
+
+export function protractorUniTestkitFactoryCreator<T>(
+  driverFactory: (base: UniDriver) => T
+) {
+  return (obj: { dataHook: string }) => {
+    const base = protractorUniDriver(
+      async () => await $(`[data-hook='${obj.dataHook}']`)
+    );
+    return driverFactory(base);
+  };
 }
