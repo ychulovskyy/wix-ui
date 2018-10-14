1463073db1099c8299d286631be2e092514e88bd
Shlomi Toussia-Cohen <shlomitc@gmail.com>
2018-10-14 12:51:16 +0300

    Update hocs.js (#786)

--- packages/wix-ui-core/hocs.js
+++ packages/wix-ui-core/hocs.js
@@ -1,5 +1,5 @@
-const { withEllipsedTooltip } = require('./src/hocs/EllipsedTooltip');
-const { withFocusable } = require('./src/hocs/Focusable');
+const { withEllipsedTooltip } = require('./dist/src/hocs/EllipsedTooltip');
+const { withFocusable } = require('./dist/src/hocs/Focusable');
 
 module.exports.withEllipsedTooltip = withEllipsedTooltip;
 module.exports.withFocusable = withFocusable;
