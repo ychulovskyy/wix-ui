ec9ba0ca513f5f56eaec87399827a3e9d40a5842
Lior Belinsky <liorbe@wix.com>
2018-10-31 09:37:07 +0200

    Fix ts issue with flush-promisses

--- packages/wix-ui-core/src/clients/GoogleMaps/GoogleMapsBasicClient.spec.ts
+++ packages/wix-ui-core/src/clients/GoogleMaps/GoogleMapsBasicClient.spec.ts
@@ -1,5 +1,5 @@
 import { GoogleMapsBasicClient } from './GoogleMapsBasicClient';
-import * as flushPromises from 'flush-promises';
+import { flushPromises } from 'flush-promises';
 
 const LANG = 'en';
 const CLIENT_ID = 'client-id';
