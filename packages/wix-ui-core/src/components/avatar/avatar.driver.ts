37ad3cb0e68f7cecd5d8a6f9a64700e9436dee2f
Erez Makavy <erezm@wix.com>
2018-11-07 10:40:56 +0200

    Remove click() from driver

--- packages/wix-ui-core/src/components/avatar/avatar.driver.ts
+++ packages/wix-ui-core/src/components/avatar/avatar.driver.ts
@@ -1,12 +1,10 @@
 import { UniDriver } from "unidriver";
-import {
-  BaseUniDriver,
-  baseUniDriverFactory
-} from "wix-ui-test-utils/base-driver";
 
 import { ContentType } from './types';
 
-export interface AvatarDriver extends BaseUniDriver {
+export interface AvatarDriver {
+  /** returns true if component exists */
+  exists: () => Promise<boolean>;
   /** Get the displayed content type. Types are: 'text', 'icon', 'image' */
   getContentType: () => Promise<ContentType>;
   /** Get the text content (generated initials) */
@@ -17,7 +15,7 @@ export const avatarDriverFactory = (base: UniDriver): AvatarDriver => {
   const getContentType = ()=> base.attr('data-content-type') as Promise<ContentType>;
 
   return {
-    ...baseUniDriverFactory(base),
+    exists: base.exists,
     getContentType,
     getTextContent: async ()=> (await getContentType()) === 'text' 
       ? base.$(':first-child').text() 
