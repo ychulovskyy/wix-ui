3cff758cc40ad8ba65570e8371bbd5ca572aab3c
Erez Makavy <erezm@wix.com>
2018-11-08 23:28:16 +0200

    Fix default theme

--- packages/wix-ui-core/src/components/avatar/avatar.story.tsx
+++ packages/wix-ui-core/src/components/avatar/avatar.story.tsx
@@ -8,6 +8,8 @@ import {
 
 import User from 'wix-ui-icons-common/User';
 
+const defaultThemeWrapper = ({component})=><div className={defaultTheme}>{component}</div>;
+
 export default {
   category: 'Components',
   storyName: 'WIP - Avatar',
@@ -17,11 +19,13 @@ export default {
   componentProps: {
     name: 'John Doe',
     'data-hook': 'storybook-avatar',
-    className: avatar()
+    className: avatar(),
   },
+  componentWrapper: defaultThemeWrapper,
   exampleProps: {
+    className: [{label: 'defaultTheme', value: avatar()}],
     icon: [{label: 'Icon1', value: <User />}],
-    imgProps: [{label: 'image1', value:{src:'http://lorempixel.com/90/90/people/'}}]
+    imgProps: [{label: 'image1', value:{src:'https://static.wixstatic.com/media/9ab0d1_8f1d1bd00e6c4bcd8764e1cae938f872~mv1.png'}}]
   }
 
 };
