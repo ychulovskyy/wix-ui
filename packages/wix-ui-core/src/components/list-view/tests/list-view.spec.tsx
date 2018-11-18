4f55065ce3e6fcb3ebb085d9b4ed0d764056fac6
gabayamir <amirga@wix.com>
2018-11-18 14:35:53 +0200

    update multi selection mouse interactions tests

--- packages/wix-ui-core/src/components/list-view/tests/list-view.spec.tsx
+++ packages/wix-ui-core/src/components/list-view/tests/list-view.spec.tsx
@@ -1,10 +1,44 @@
 import * as React from 'react';
 import {times, partition} from 'lodash';
-import {createDriver, SimulateCtrlKey} from './list-view.driver';
+import {createDriver, SimulateCtrlKey, SimulateCtrlShiftKey, SimulateShiftKey} from './list-view.driver';
 import {mount} from "enzyme";
 import {ListView} from "../list-view";
 import {ListViewDefaultState, ListViewRenderItemProps, ListViewSelectionType, ListViewState} from "../list-view-types";
 import Mock = jest.Mock;
+
+expect.extend({
+    toHaveSameItems (receivedArr, targetArr) {
+
+        let pass = false;
+
+        if (!receivedArr || !targetArr)
+        {
+            pass = receivedArr === targetArr;
+        }
+        else if (receivedArr.length === targetArr.length)
+        {
+            const targetSet = new Set(targetArr);
+
+            pass = true;
+
+            for (let item of receivedArr)
+            {
+                if (!targetSet.has(item))
+                {
+                    pass = false;
+                    break;
+                }
+            }
+        }
+
+        return {
+            message: () =>
+                `expected ${receivedArr} to have the same items as ${targetArr}`,
+            pass: pass,
+        }
+    },
+});
+
 
 describe('ListView', () => {
 
@@ -14,7 +48,10 @@ describe('ListView', () => {
     let renderItem;
     let onChange;
 
+
+
     beforeAll(() => {
+
         const dataSourceItemsCount = 12;
 
         // We'll create a data source that will contain items for ListView.
@@ -147,101 +184,199 @@ describe('ListView', () => {
 
     describe('ListView with multiple selection', () => {
 
-        beforeAll(() => {
-            listView.setProps({
-                selectionType: ListViewSelectionType.Multiple,
-                listViewState: ListViewDefaultState
+        describe('ListView multiple selection mouse interaction', () => {
+
+            beforeAll(() => {
+                listView.setProps({
+                    selectionType: ListViewSelectionType.Multiple,
+                    listViewState: ListViewDefaultState
+                });
+            });
+
+            it(`Should select a selectable item when it's clicked`, () => {
+
+                listViewDriver.itemClick(1);
+
+                expectStateChange(onChange, {
+                    selectedIds: [1],
+                    selectionStartId: 1,
+                    currentNavigatableItemId: 1,
+                });
+
+                expectRerendering(renderItem, [
+                    {
+                        dataItemId: 1,
+                        isSelected: true,
+                        isCurrent: true
+                    }
+                ])
+            });
+
+            it(`Should add to selection when a different selectable item is clicked with ctrl`, () => {
+                listViewDriver.itemClick(3, SimulateCtrlKey);
+
+                expectStateChange(onChange, {
+                    selectedIds: [1,3],
+                    selectionStartId: 3,
+                    currentNavigatableItemId: 3,
+                });
+
+                expectRerendering(renderItem, [
+                    {
+                        dataItemId: 1,
+                        isSelected: true,
+                        isCurrent: false
+                    },
+                    {
+                        dataItemId: 3,
+                        isSelected: true,
+                        isCurrent: true
+                    }
+                ])
+            });
+
+            it (`Should move the current item but leaves the selection intact when clicking witch ctrl a non selectable item.`, () => {
+
+                listViewDriver.itemClick(2, SimulateCtrlKey);
+
+                expectStateChange(onChange, {
+                    selectedIds: [1,3],
+                    selectionStartId: 3,
+                    currentNavigatableItemId: 2,
+                });
+
+                expectRerendering(renderItem, [
+                    {
+                        dataItemId: 2,
+                        isSelected: false,
+                        isCurrent: true
+                    },
+                    {
+                        dataItemId: 3,
+                        isSelected: true,
+                        isCurrent: false
+                    }
+                ])
+            });
+
+            it(`Should remove from selection when a selected item is clicked with ctrl`, () => {
+                listViewDriver.itemClick(3, SimulateCtrlKey);
+
+                expectStateChange(onChange, {
+                    selectedIds: [1],
+                    selectionStartId: 3,
+                    currentNavigatableItemId: 3,
+                });
+
+                expectRerendering(renderItem, [
+                    {
+                        dataItemId: 2,
+                        isSelected: false,
+                        isCurrent: false
+                    },
+                    {
+                        dataItemId: 3,
+                        isSelected: false,
+                        isCurrent: true
+                    }
+                ])
+            });
+
+            it(`Should keep current selection and add to selection selectable items from selection start to clicked item when item is clicked with ctrl+shift`, () => {
+                listViewDriver.itemClick(7, SimulateCtrlShiftKey);
+
+                expectStateChange(onChange, {
+                    selectedIds: [1, 3, 5, 7],
+                    selectionStartId: 3,
+                    currentNavigatableItemId: 7,
+                });
+
+                expectRerendering(renderItem, [
+                    {
+                        dataItemId: 3,
+                        isSelected: true,
+                        isCurrent: false
+                    },
+                    {
+                        dataItemId: 5,
+                        isSelected: true,
+                        isCurrent: false
+                    },
+                    {
+                        dataItemId: 7,
+                        isSelected: true,
+                        isCurrent: true
+                    }
+                ])
+            });
+
+            it(`Should select all selectable items in range between selection start to last clicked item when item is clicked with shift`, () => {
+                listViewDriver.itemClick(11, SimulateShiftKey);
+
+                expectStateChange(onChange, {
+                    selectedIds: [3, 5, 7, 9, 11],
+                    selectionStartId: 3,
+                    currentNavigatableItemId: 11,
+                });
+
+                expectRerendering(renderItem, [
+                    {
+                        dataItemId: 1,
+                        isSelected: false,
+                        isCurrent: false
+                    },
+                    {
+                        dataItemId: 7,
+                        isSelected: true,
+                        isCurrent: false
+                    },
+                    {
+                        dataItemId: 9,
+                        isSelected: true,
+                        isCurrent: false
+                    },
+                    {
+                        dataItemId: 11,
+                        isSelected: true,
+                        isCurrent: true
+                    }
+                ])
+            });
+
+            it(`Should unselect all selectable items and select only clicked item`, () => {
+                listViewDriver.itemClick(11, SimulateShiftKey);
+
+                expectStateChange(onChange, {
+                    selectedIds: [3, 5, 7, 9, 11],
+                    selectionStartId: 3,
+                    currentNavigatableItemId: 11,
+                });
+
+                expectRerendering(renderItem, [
+                    {
+                        dataItemId: 1,
+                        isSelected: false,
+                        isCurrent: false
+                    },
+                    {
+                        dataItemId: 7,
+                        isSelected: true,
+                        isCurrent: false
+                    },
+                    {
+                        dataItemId: 9,
+                        isSelected: true,
+                        isCurrent: false
+                    },
+                    {
+                        dataItemId: 11,
+                        isSelected: true,
+                        isCurrent: true
+                    }
+                ])
             });
         });
 
-        it(`Should select a selectable item when it's clicked`, () => {
-
-            listViewDriver.itemClick(1);
-
-            expectStateChange(onChange, {
-                selectedIds: [1],
-                selectionStartId: 1,
-                currentNavigatableItemId: 1,
-            });
-
-            expectRerendering(renderItem, [
-                {
-                    dataItemId: 1,
-                    isSelected: true,
-                    isCurrent: true
-                }
-            ])
-        });
-
-        it(`Should add to selection when a different selectable item is clicked with ctrl`, () => {
-            listViewDriver.itemClick(3, SimulateCtrlKey);
-
-            expectStateChange(onChange, {
-                selectedIds: [1,3],
-                selectionStartId: 3,
-                currentNavigatableItemId: 3,
-            });
-
-            expectRerendering(renderItem, [
-                {
-                    dataItemId: 1,
-                    isSelected: true,
-                    isCurrent: false
-                },
-                {
-                    dataItemId: 3,
-                    isSelected: true,
-                    isCurrent: true
-                }
-            ])
-        });
-
-        it (`Should move the current item but leaves the selection intact when clicking witch ctrl a non selectable item.`, () => {
-
-            listViewDriver.itemClick(2, SimulateCtrlKey);
-
-            expectStateChange(onChange, {
-                selectedIds: [1,3],
-                selectionStartId: 3,
-                currentNavigatableItemId: 2,
-            });
-
-            expectRerendering(renderItem, [
-                {
-                    dataItemId: 2,
-                    isSelected: false,
-                    isCurrent: true
-                },
-                {
-                    dataItemId: 3,
-                    isSelected: true,
-                    isCurrent: false
-                }
-            ])
-        });
-
-        it(`Should remove from selection when a selected item is clicked with ctrl`, () => {
-            listViewDriver.itemClick(3, SimulateCtrlKey);
-
-            expectStateChange(onChange, {
-                selectedIds: [1],
-                selectionStartId: 3,
-                currentNavigatableItemId: 3,
-            });
-
-            expectRerendering(renderItem, [
-                {
-                    dataItemId: 2,
-                    isSelected: false,
-                    isCurrent: false
-                },
-                {
-                    dataItemId: 3,
-                    isSelected: false,
-                    isCurrent: true
-                }
-            ])
-        });
     });
 
     describe('ListView with none selection', () => {
@@ -312,9 +447,32 @@ describe('ListView', () => {
         expect(onChangeMock).toHaveBeenCalledTimes(0);
     }
 
-    function expectStateChange (onChangeMock: Mock, listViewState: Partial<ListViewState>) {
+    function expectStateChange (onChangeMock: Mock, expectedListViewState: Partial<ListViewState>) {
         expect(onChangeMock).toHaveBeenCalledTimes(1);
-        expect(onChangeMock).toBeCalledWith(expect.objectContaining(listViewState));
+
+        const {
+            selectedIds: expectedSelectedIds,
+            disabledIds: expectedDisabledIds,
+            ...expectedShallowListViewStateProps
+        } = expectedListViewState;
+
+        const calls = onChangeMock.mock.calls;
+        const singleCall = calls[0];
+        const updatedState = singleCall[0];
+
+
+        if (expectedSelectedIds !== undefined)
+        {
+            expect(expectedSelectedIds).toHaveSameItems(updatedState.selectedIds);
+        }
+
+
+        if (expectedDisabledIds !== undefined)
+        {
+            expect(expectedDisabledIds).toHaveSameItems(updatedState.disabledIds);
+        }
+
+        expect(onChangeMock).toBeCalledWith(expect.objectContaining(expectedShallowListViewStateProps));
     }
 
     function expectRerendering (renderMock: Mock, renderItemsPropsArr: Array<Partial<ListViewRenderItemProps<any, any>>>) {
