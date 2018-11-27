a8e19c30776e0b33e584df2a460e526cfc2e70d7
Yarden Sod-Moriah <yardnsm@gmail.com>
2018-11-27 16:06:54 +0200

    <Popover/> - fix mock to pass the build

--- packages/wix-ui-core/src/components/Popover/__mocks__/popper.js.ts
+++ packages/wix-ui-core/src/components/Popover/__mocks__/popper.js.ts
@@ -1,11 +1,8 @@
 /* tslint:disable:no-empty */
-import * as PopperJs from 'popper.js';
 
 let scheduleUpdateMock = () => {};
 
 export default class Popper {
-  static placements = PopperJs.placements;
-
   static __setScheduleUpdateImplementation(impl) {
     scheduleUpdateMock = impl;
   }
