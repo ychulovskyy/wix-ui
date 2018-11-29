b64963e17f17ad0a5c165ea34ffed5c8da633997
Rashelle Kosiborod <rashellek@wixs-MacBook-Pro-3.local>
2018-11-29 16:46:51 +0200

    nav-stepper change

--- packages/wix-ui-core/stories/AddressInput/E2E.tsx
+++ packages/wix-ui-core/stories/AddressInput/E2E.tsx
@@ -3,7 +3,7 @@ import {storiesOf} from '@storybook/react';
 import {AddressInput} from '../../src/components/AddressInput';
 import {GoogleMapsClientStub} from '../../src/components/AddressInput/GoogleMapsClientStub';
 import {MapsClientConstructor} from '../../src/clients/GoogleMaps/types';
-import * as helper from '../../src/components/AddressInput/AddressInputTestHelper';
+import * as helper from '../../src/components/AddressInput/address-input-test-helper';
 
 GoogleMapsClientStub.setAddresses([helper.ADDRESS_1, helper.ADDRESS_2]);
 GoogleMapsClientStub.setGeocode(helper.GEOCODE_1);
