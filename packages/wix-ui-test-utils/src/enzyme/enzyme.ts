ae49bfd8b11f60a6761c6e2851067edd7847a484
Mykolas Sindeikis <mykolass@wix.com>
2018-12-12 14:04:09 +0200

    Revert "enzymeTestkitFactoryCreator - adding new function parameter `upgrade`" (#1025)
    * Revert "`<Popover/>` Driver - Fix clickOutside signature (#1024)"
    
    This reverts commit 91ab3d9c30375747efe518e0ad4b737670307ffd.
    
    * Revert "Add upgrade value to support switching old and new testkits (#1023)"
    
    This reverts commit 36e347a3a6e14de564cc85207b50c58493d3cc52.

--- packages/wix-ui-test-utils/src/enzyme/enzyme.ts
+++ packages/wix-ui-test-utils/src/enzyme/enzyme.ts
@@ -9,11 +9,10 @@ import {reactUniDriver} from 'unidriver/react';
 export interface WrapperData {
   wrapper: ReactWrapper;
   dataHook: string;
-  upgrade?: boolean;
 }
 
 export type MountFunctionType = (node: React.ReactElement<any>, options?: MountRendererProps) => ReactWrapper;
-export type EnzymeDriverFactory<T extends BaseDriver> = (data: {element: Element | undefined, wrapper: ReactWrapper, eventTrigger: any, upgrade?: boolean}) => T;
+export type EnzymeDriverFactory<T extends BaseDriver> = (data: {element: Element | undefined, wrapper: ReactWrapper, eventTrigger: any}) => T;
 
 export function enzymeTestkitFactoryCreator<T extends BaseDriver> (driverFactory: EnzymeDriverFactory<T>) {
   return (obj: WrapperData) => {
@@ -21,7 +20,7 @@ export function enzymeTestkitFactoryCreator<T extends BaseDriver> (driverFactory
     const regexp = new RegExp(`^<[^>]+data-hook="${obj.dataHook}"`);
     const component = obj.wrapper.findWhere(n => n.length > 0 && typeof n.type() === 'string' && (regexp).test(n.html()));
     const element = component.length > 0 ? component.first().getDOMNode() : undefined;
-    return driverFactory({element, wrapper: obj.wrapper, eventTrigger, upgrade: obj.upgrade});
+    return driverFactory({element, wrapper: obj.wrapper, eventTrigger});
   };
 }
 
